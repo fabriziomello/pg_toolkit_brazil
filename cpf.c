b92fe36340d1e6a68caa064f0b80c2c7710d1e61
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2023-02-06 17:39:30 -0300

    Added more CPF validations
    1) Size validation: CPF should not be less than 100 and greater than
       99999999999.
    2) CPF with all digits equal are not allowed (i.e. 11111111111).
    
    Also added better error messages using proper error code.
    
    Signed-off-by: Fabrízio de Royes Mello <fabriziomello@gmail.com>

--- cpf.c
+++ cpf.c
@@ -10,15 +10,15 @@ PG_FUNCTION_INFO_V1(cpfout);
 Datum
 cpfout(PG_FUNCTION_ARGS)
 {
-	int64   val = PG_GETARG_INT64(0);
-    int     group[4];
-	char    *result;
+	int64		val = PG_GETARG_INT64(0);
+	int			group[4];
+	char	   *result;
 
-    /* 000.000.000-00 */
-	group[0] = (val / 100000000) % 1000;    /* first group of three digits */
-	group[1] = (val / 100000) % 1000;       /* second group of three digits */
-	group[2] = (val / 100) % 1000;          /* third group of three digits */
-	group[3] = (val % 100);                 /* check digits (last two digits)*/
+	/* 000.000.000-00 */
+	group[0] = (val / 100000000) % 1000;	/* first group of three digits */
+	group[1] = (val / 100000) % 1000;	/* second group of three digits */
+	group[2] = (val / 100) % 1000;	/* third group of three digits */
+	group[3] = (val % 100);		/* check digits (last two digits) */
 
 	result = psprintf("%03d.%03d.%03d-%02d", group[0], group[1], group[2], group[3]);
 
@@ -29,18 +29,21 @@ cpfout(PG_FUNCTION_ARGS)
 static int
 compute_cpf_check_digits(long int cpf)
 {
-	int i = 0, d = 0, d1 = 0, d2 = 0;
+	int			i = 0,
+				d = 0,
+				d1 = 0,
+				d2 = 0;
 
 	/* remove last two digits */
 	cpf = cpf / 100;
 
-    do
-    {
+	do
+	{
 		d = cpf % 10;
-        d1 = d1 + d * (9 - (i % 10));
+		d1 = d1 + d * (9 - (i % 10));
 		d2 = d2 + d * (9 - ((i++ + 1) % 10));
 		cpf = cpf / 10;
-    } while (cpf > 0);
+	} while (cpf > 0);
 
 	d1 = (d1 % 11) % 10;
 	d2 = d2 + d1 * 9;
@@ -49,10 +52,41 @@ compute_cpf_check_digits(long int cpf)
 	return (d1 * 10) + d2;
 }
 
-static bool
-check_cpf_digits(int64 cpf)
+static void
+validate_cpf(int64 cpf)
 {
-    return compute_cpf_check_digits(cpf) == (cpf % 100);
+	char	   *out;
+	int			i;
+
+	/* Check sizes */
+	if (cpf < 100 || cpf > 99999999999)
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_WRONG_OBJECT_TYPE),
+				 errmsg("invalid CPF"),
+				 errdetail("CPF should not be less than 100 and greater than 99999999999.")));
+	}
+
+	/* Check if all digits are the same. */
+	out = psprintf("%011ld", cpf);
+	for (i = 1; i < 11 && out[i] == out[0]; i++)
+	{
+		if (i == 10)
+		{
+			ereport(ERROR,
+					(errcode(ERRCODE_WRONG_OBJECT_TYPE),
+					 errmsg("invalid CPF"),
+					 errdetail("All CPF digits should not be equal.")));
+		}
+	}
+
+	if (compute_cpf_check_digits(cpf) != (cpf % 100))
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_WRONG_OBJECT_TYPE),
+				 errmsg("invalid CPF"),
+				 errdetail("Invalid check digit for the CPF.")));
+	}
 }
 
 PG_FUNCTION_INFO_V1(cpfin);
@@ -60,12 +94,9 @@ PG_FUNCTION_INFO_V1(cpfin);
 Datum
 cpfin(PG_FUNCTION_ARGS)
 {
-    int64 value = DirectFunctionCall1(int8in, PG_GETARG_DATUM(0));
+	int64		value = DirectFunctionCall1(int8in, PG_GETARG_DATUM(0));
 
-    if (!check_cpf_digits(value))
-    {
-        elog(ERROR, "CPF inválido");
-    }
+	validate_cpf(value);
 
 	PG_RETURN_INT64(value);
 }
