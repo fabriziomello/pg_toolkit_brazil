81e6d64f1a77db2070dd8bc9fd639daf2be18512
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2023-02-06 18:45:37 -0300

    Added basic operators '=', '<>', '<', '<=', '>=' and '>'
    Signed-off-by: Fabrízio de Royes Mello <fabriziomello@gmail.com>

--- pg_toolkit_brazil--0.1.sql
+++ pg_toolkit_brazil--0.1.sql
@@ -8,6 +8,7 @@ CREATE TYPE cpf;
 --
 --  Input and output functions.
 --
+
 CREATE FUNCTION cpfin(cstring)
 RETURNS cpf
 AS 'MODULE_PATHNAME'
@@ -31,6 +32,7 @@ LANGUAGE internal STABLE STRICT PARALLEL SAFE;
 --
 --  Type definition
 --
+
 CREATE TYPE cpf (
     INPUT   = cpfin,
     OUTPUT  = cpfout,
@@ -42,4 +44,105 @@ CREATE TYPE cpf (
 --
 --  Implicit and assignment type casts.
 --
+
 CREATE CAST (bigint AS cpf) WITHOUT FUNCTION AS IMPLICIT;
+
+--
+-- Operator Functions.
+--
+
+CREATE FUNCTION cpf_eq( cpf, cpf )
+RETURNS bool
+AS 'int8eq'
+LANGUAGE internal IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION cpf_ne( cpf, cpf )
+RETURNS bool
+AS 'int8ne'
+LANGUAGE internal IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION cpf_lt( cpf, cpf )
+RETURNS bool
+AS 'int8lt'
+LANGUAGE internal IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION cpf_le( cpf, cpf )
+RETURNS bool
+AS 'int8le'
+LANGUAGE internal IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION cpf_gt( cpf, cpf )
+RETURNS bool
+AS 'int8gt'
+LANGUAGE internal IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION cpf_ge( cpf, cpf )
+RETURNS bool
+AS 'int8ge'
+LANGUAGE internal IMMUTABLE STRICT PARALLEL SAFE;
+
+--
+-- Operators.
+--
+
+CREATE OPERATOR = (
+    LEFTARG    = CPF,
+    RIGHTARG   = CPF,
+    COMMUTATOR = =,
+    NEGATOR    = <>,
+    PROCEDURE  = cpf_eq,
+    RESTRICT   = eqsel,
+    JOIN       = eqjoinsel,
+    HASHES,
+    MERGES
+);
+
+CREATE OPERATOR <> (
+    LEFTARG    = CPF,
+    RIGHTARG   = CPF,
+    NEGATOR    = =,
+    COMMUTATOR = <>,
+    PROCEDURE  = cpf_ne,
+    RESTRICT   = neqsel,
+    JOIN       = neqjoinsel
+);
+
+CREATE OPERATOR < (
+    LEFTARG    = CPF,
+    RIGHTARG   = CPF,
+    NEGATOR    = >=,
+    COMMUTATOR = >,
+    PROCEDURE  = cpf_lt,
+    RESTRICT   = scalarltsel,
+    JOIN       = scalarltjoinsel
+);
+
+CREATE OPERATOR <= (
+    LEFTARG    = CPF,
+    RIGHTARG   = CPF,
+    NEGATOR    = >,
+    COMMUTATOR = >=,
+    PROCEDURE  = cpf_le,
+    RESTRICT   = scalarltsel,
+    JOIN       = scalarltjoinsel
+);
+
+CREATE OPERATOR >= (
+    LEFTARG    = CPF,
+    RIGHTARG   = CPF,
+    NEGATOR    = <,
+    COMMUTATOR = <=,
+    PROCEDURE  = cpf_ge,
+    RESTRICT   = scalargtsel,
+    JOIN       = scalargtjoinsel
+);
+
+CREATE OPERATOR > (
+    LEFTARG    = CPF,
+    RIGHTARG   = CPF,
+    NEGATOR    = <=,
+    COMMUTATOR = <,
+    PROCEDURE  = cpf_gt,
+    RESTRICT   = scalargtsel,
+    JOIN       = scalargtjoinsel
+);
