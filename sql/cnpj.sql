2e14ff24689c44e26e274e2048bbff9d9080c5a3
Jose Arthur Benetasso Villanova <jose.arthur@gmail.com>
2023-02-09 14:09:07 -0300

    Make better tests

--- sql/cnpj.sql
+++ sql/cnpj.sql
@@ -1,15 +1,33 @@
 CREATE TABLE empresa(cnpj cnpj);
 \d empresa
-INSERT INTO empresa VALUES (cnpj '00000000000191');
-SELECT * FROM empresa;
 
-DELETE FROM empresa;
+-- Banco do Brasil's cnpj
+INSERT INTO empresa VALUES (cnpj '00000000000191');
+-- Petrobras' cnpj
 INSERT INTO empresa VALUES (cnpj '33000167066129');
-SELECT * FROM empresa;
+-- Eletrobras' cnpj
+INSERT INTO empresa VALUES (cnpj '00.001.180/0001-26');
+SELECT * FROM empresa ORDER BY cnpj;
+
+-- Invalid digit
 SELECT cnpj '00000000000001';
+
+-- cnpj bigger than expected
 SELECT cnpj '100000000000191';
+
+-- Formatted cnpj
 SELECT cnpj '33.000.167/0661-29';
+
+-- Invalid format
 SELECT cnpj '33000.167/0661-29';
-SELECT CNPJ '00000000000191' = CNPJ '00.000.000/0001-91';
-SELECT CNPJ '00000000000191' = CNPJ '00.001.180/0001-26';
-SELECT CNPJ '00000000000191' < CNPJ '00.001.180/0001-26';
+
+-- bigint cast
+SELECT 33000167066129::cnpj;
+
+-- operators
+SELECT cnpj '00000000000191' = cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' <> cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' < cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' <= cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' >= cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' > cnpj '00.001.180/0001-26';
