c88e67c5b2cf5e56fe698355b96a9d99d298db19
José Arthur Benetasso Villanova <jose.arthur@gmail.com>
2023-02-10 15:27:28 -0300

    Add CNPJ datatype
    Added the CNPJ datatype along with implicit cast from bigint, basic operators
    and btree indexing support.
    
    Co-authored-by: Fabrízio de Royes Mello <fabriziomello@gmail.com>

---
+++ sql/cnpj.sql
@@ -0,0 +1,33 @@
+CREATE TABLE empresa(cnpj cnpj PRIMARY KEY);
+\d empresa
+
+-- Banco do Brasil's cnpj
+INSERT INTO empresa VALUES (cnpj '00000000000191');
+-- Petrobras' cnpj
+INSERT INTO empresa VALUES (cnpj '33000167066129');
+-- Eletrobras' cnpj
+INSERT INTO empresa VALUES (cnpj '00.001.180/0001-26');
+SELECT * FROM empresa ORDER BY cnpj;
+
+-- Invalid digit
+SELECT cnpj '00000000000001';
+
+-- cnpj bigger than expected
+SELECT cnpj '100000000000191';
+
+-- Formatted cnpj
+SELECT cnpj '33.000.167/0661-29';
+
+-- Invalid format
+SELECT cnpj '33000.167/0661-29';
+
+-- bigint cast
+SELECT 33000167066129::cnpj;
+
+-- operators
+SELECT cnpj '00000000000191' = cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' <> cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' < cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' <= cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' >= cnpj '00.001.180/0001-26';
+SELECT cnpj '00000000000191' > cnpj '00.001.180/0001-26';
